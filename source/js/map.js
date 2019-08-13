9a0dcfc8b63c156c7c5d9f1d5048b7416617825e
Ksenia <kpuchkova@gmail.com>
2019-08-13 17:38:32 +0300

    Тревис

--- source/js/map.js
+++ source/js/map.js
@@ -1,46 +1,46 @@
 ymaps.ready(init);
-   var myMap;
+  var myMap;
 
-   function init() {
-     if (document.body.clientWidth < 768) {
-       myMap = new ymaps.Map("map", {
-         center: [59.938640, 30.323055],
-         zoom: 16
-       });
+  function init() {
+    if (document.body.clientWidth < 768) {
+      myMap = new ymaps.Map("map", {
+        center: [59.938640, 30.323055],
+        zoom: 16
+      });
 
-       var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {}, {
-         iconLayout: 'default#image',
-         iconImageHref: 'img/map-pin.png',
-         iconImageSize: [80, 68],
-         iconImageOffset: [-36, -53]
-       });
+      var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/map-pin.png',
+        iconImageSize: [80, 68],
+        iconImageOffset: [-36, -53]
+      });
 
-     } else if (document.body.clientWidth >= 768 && document.body.clientWidth < 1300){
-       myMap = new ymaps.Map("map", {
-         center: [59.938640, 30.323055],
-         zoom: 17
-       });
+    } else if (document.body.clientWidth >= 768 && document.body.clientWidth < 1300){
+      myMap = new ymaps.Map("map", {
+        center: [59.938640, 30.323055],
+        zoom: 17
+      });
 
-       var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {}, {
-         iconLayout: 'default#image',
-         iconImageHref: 'img/map-pin.png',
-         iconImageSize: [117, 100],
-         iconImageOffset: [-72, -106]
-       });
+      var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/map-pin.png',
+        iconImageSize: [117, 100],
+        iconImageOffset: [-72, -106]
+      });
 
-     } else {
-       myMap = new ymaps.Map("map", {
-         center: [59.938963, 30.319293],
-         zoom: 17
-       });
+    } else {
+      myMap = new ymaps.Map("map", {
+        center: [59.938963, 30.319293],
+        zoom: 17
+      });
 
-       var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {}, {
-         iconLayout: 'default#image',
-         iconImageHref: 'img/map-pin.png',
-         iconImageSize: [117, 100],
-         iconImageOffset: [-72, -106]
-       });
-     }
+      var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/map-pin.png',
+        iconImageSize: [117, 100],
+        iconImageOffset: [-72, -106]
+      });
+    }
 
-     myMap.geoObjects.add(myPlacemark);
-   }
+    myMap.geoObjects.add(myPlacemark);
+  }
